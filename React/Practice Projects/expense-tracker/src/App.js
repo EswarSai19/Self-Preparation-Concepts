6aa47c5df2dbfb0f8187ec3758ee49c293840c52
EswarSai19 <eswarsai219@gmail.com>
2023-12-18 08:54:32 +0530

    expense trackrt under progress

---
+++ React/Practice Projects/expense-tracker/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Balance from "./Components/Balance";
+import IncomeExpenses from "./Components/IncomeExpenses";
+import TransactionList from "./Components/TransactionList";
+import AddTransaction from "./Components/AddTransaction";
+import { GlobalProvider } from "./Context/GlobalState";
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div class="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
