a90a8e8b1e826d4eb1bd3642da25b1ab5aa2cf90
EswarSai19 <eswarsai219@gmail.com>
2024-01-02 10:04:49 +0530

    portfolio under progress

---
+++ React/Portfolio/my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import Intro from "./Components/Intro/intro";
+import Navbar from "./Components/Navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Portfolio/my-portfolio/src/Components/Intro/intro.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./intro.css";
+import bg from "../../assets/image.png";
+import btnImg from "../../assets/hireme.png";
+import { Link } from "react-scroll";
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello</span>
+        <span className="introText">
+          I'm<span className="introName">Eswar</span>
+          <br />
+          <span>Website Designer</span>
+        </span>
+        <p className="introPara">
+          I am a skilled web developer with experience in creating various
+          things which are new.
+        </p>
+        <Link>
+          <button className="btn">
+            <img src={btnImg} alt="" />
+            Hire Me
+          </button>
+        </Link>
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
+};
+
+export default Intro;
