230f8e223c7f81434e954e70f8b193db8f90d925
EswarSai19 <eswarsai219@gmail.com>
2024-01-06 10:06:25 +0530

    new changes under progress

--- React/Combined Projects/E-Commerce-Website/Frontend/src/Context/ShopContext.jsx
+++ React/Combined Projects/E-Commerce-Website/Frontend/src/Context/ShopContext.jsx
@@ -1,8 +1,46 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_product from "../Components/Assets/all_product";
 export const ShopContext = createContext(null);
+const getDefaultCart = () => {
+  let cart = {};
+  for (let i = 0; i < all_product.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
+};
 const ShopContextProvider = (props) => {
-  const contextValue = { all_product };
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    console.log(cartItems);
+  };
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    console.log("Cart Items", cartItems);
+    let totalAmount = 0;
+    for (let item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        console.log("new price", itemInfo.new_price);
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+      console.log("total amount:", totalAmount);
+      return totalAmount;
+    }
+  };
+
+  const contextValue = {
+    all_product,
+    getTotalCartAmount,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
